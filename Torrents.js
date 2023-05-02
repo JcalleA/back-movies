9de2d3101eb0771f5c19ea91b5d3abff8d2020fb
JcalleA <kalle-1413@hotmail.com>
2023-05-02 15:02:55 -0500

    ok

--- Torrents.js
+++ Torrents.js
@@ -1,12 +1,13 @@
 const TorrentSearchApi = require('torrent-search-api');
+const t6 = 'ThePirateBay';  //categories: ['All','Audio','Video','Applications','Games','Porn','Other','Top100']
 
-TorrentSearchApi.enableProvider('Torrent9');
+TorrentSearchApi.enableProvider(t6);
 
 // Search '1080' in 'Movies' category and limit to 20 results
 exports.torrents = async (req, res) => {
     const query = req.params.query
     try{
-        const torrents = await TorrentSearchApi.search(query, 'Movies', 20);
+        const torrents = await TorrentSearchApi.search(query, 'Video', 100);
         return(res.json(torrents))
     }catch{
         return(res.json({error:"error"}))
--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const express = require("express");
 const cors = require("cors");
 const app=express();
-const PORT= process.env.PORT || 3000;
 const torrents = require("./Torrents");
-app.get("/:query",torrents.torrents);
+
+const PORT= process.env.PORT || 3000;
+
+
+app.use(cors());
+
+app.get("/:query", torrents.torrents);
 app.listen(PORT, function () {
       console.log(`Example app listening on port ${PORT}!`);
       });
